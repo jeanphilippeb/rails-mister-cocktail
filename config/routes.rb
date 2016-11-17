e62cb9baa258da78d6f1f89caa086852f91c4e65
Jean-Philippe BOUL <jean.philippe.boul@gmail.com>
2016-11-17 14:27:27 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [ :index, :new, :create, :show ]  do
+    resources :doses, only: [ :index, :new, :create, :delete ]
+    resources :ingredients, only: [ :index ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
